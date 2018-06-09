003c5968baad3cbb98756dc1da3e8518980a1eb2
akaminet@umich.edu <akaminet@umich.edu>
2018-06-09 17:14:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ function removeFromCart(item) {
     return "That item is not in your cart."
   }
   else{
-    cart.prototype.splice(key, 1)
+    cart.prototype.splice(1, key)
   }
   return cart
 }
