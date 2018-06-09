8ee9e8c5e8f331cc0a4603d52ff68a8f6b7cc10b
akaminet@umich.edu <akaminet@umich.edu>
2018-06-09 17:46:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -71,5 +71,11 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if(cardNumber === null){
+    console.log("Sorry, we don't have a credit card on file for you.")
+  }
+  else{
+    var str = `Your total cost is ${cart.total()}, which will be charged to the card ${cardNumber}`
+    
+  }
 }
